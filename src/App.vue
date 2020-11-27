9c82d499422e766f7724356ddd7c54935ed54952
Michael Anderson <ratboyjim@gmail.com>
2020-11-27 13:42:10 +0000

    first test passing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <div>
+    <h1>Films</h1>
+
+   <label for="film-select">Select a Film:</label>
+    <select id="film-select" v-model="selectedFilm">
+      <option disabled value="">Select a Film</option>
+      <option v-for="(film, index) in films" :key="index" :value="film">{{film.title}}</option>
+    </select>
+
+
+  </div>
 </template>
 
 <script>
@@ -12,6 +22,19 @@ export default {
     return {
       films: []
     }
+  },
+  components: {
+
+  },
+  methods: {
+    fetchData(){
+      fetch('https://ghibliapi.herokuapp.com/films')
+      .then(response => response.json())
+      .then(data => this.films = data)
+    }
+  },
+  mounted() {
+    this.fetchData()
   }
 
 }
