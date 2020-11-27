f25d8df29c79784489c010b6cbfffcf6c88db55a
Michael Anderson <ratboyjim@gmail.com>
2020-11-27 12:27:59 +0000

    first test written

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,21 @@
 </template>
 
 <script>
+
+import { eventBus } from '@/main.js';
+
 export default {
+  name: 'app',
+  data(){
+    return {
+      films: []
+    }
+  }
 
 }
 </script>
+
+
 
 <style>
 
